04dcde3ff2cc644761af5045bc94dc1cfec955d8
kevinjunsang <m263636@usna.edu>
2023-10-12 09:43:46 -0400

    Created the background

--- game.py
+++ game.py
@@ -1,2 +1,16 @@
-print("Hello world!")
-print("new message!")
+import pygame
+import time
+
+pygame.init()
+
+screen = pygame.display.set_mode((400, 400))
+pygame.display.set_caption("CHOMP!")
+
+screen.fill((0,0,255))
+pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400))
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("Nope not today, this game runs 4eva")
