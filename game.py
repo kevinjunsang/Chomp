5790eb0ad0543d98885c60de863db0c0d6613605
kevinjunsang <m263636@usna.edu>
2023-10-13 11:16:17 -0400

    Added fish

--- game.py
+++ game.py
@@ -6,9 +6,12 @@ pygame.init()
 
 screen = pygame.display.set_mode((400, 400))
 pygame.display.set_caption("CHOMP!")
+screen.fill((114, 159, 232))
 
-screen.fill((0,0,255))
 pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400))
+fish = pygame.image.load("assets/images/fishTile_081.png").convert()
+sand = pygame.image.load("assets/images/sand.png").convert()
+screen.blit(fish, (200,200,64,64))
 pygame.display.flip()
 
 while True:
