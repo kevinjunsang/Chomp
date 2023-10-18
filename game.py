c13bb022ad92eb522cd407f33a445211c0be95f8
kevinjunsang <m263636@usna.edu>
2023-10-18 09:27:34 -0400

    use parametric variables

--- game.py
+++ game.py
@@ -1,17 +1,31 @@
 import pygame
 import sys
-import time
+
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
+SAND_HEIGHT = 20
+TILE_SIZE = 64  # tiles are squares height == width
+
+# COLORS
+WATER_COLOR = (114, 159, 232)
+SAND_COLOR = (100, 25, 0)
 
 pygame.init()
 
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("CHOMP!")
-screen.fill((114, 159, 232))
+screen.fill(WATER_COLOR)
 
-pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400))
+pygame.draw.rect(screen, SAND_COLOR,
+                 (0, SCREEN_HEIGHT - SAND_HEIGHT,
+                  SCREEN_WIDTH, SAND_HEIGHT))
 fish = pygame.image.load("assets/images/fishTile_081.png").convert()
 sand = pygame.image.load("assets/images/sand.png").convert()
-screen.blit(fish, (200,200,64,64))
+screen.blit(fish,
+            (SCREEN_WIDTH / 2 - TILE_SIZE / 2,
+             SCREEN_HEIGHT / 2 - TILE_SIZE / 2))
+# can designate by just identifying the top left corner
+# pygame.draw.rect(screen, (0, 255, 0), (200, 200, 50, 50))
 pygame.display.flip()
 
 while True:
@@ -21,3 +35,5 @@ while True:
             pygame.quit()
             print("Thanks for playing")
             sys.exit()
+
+        pygame.display.flip()
